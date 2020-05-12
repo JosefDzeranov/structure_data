fc7ae7b2af1ab8ec4a172391d380bd76707d5424
Josef Dzeranov <soslan29@gmail.com>
2020-05-12 08:04:18 +0300

    Add SinglyLinkedList

---
+++ StructureData/SinglyLinkedList/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace SinglyLinkedList
+{
+    public class Node
+    {
+        public Node Next;
+        public int Value;
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ StructureData/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace SinglyLinkedList
+{
+    public class SinglyLinkedList
+    {
+        public Node Head;
+        private int count = 0;
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public string Print()
+        {
+            string result = "";
+            Node current = Head;
+            while (current != null)
+            {
+                result += current.Value + " ";
+                current = current.Next;
+            }
+
+            return result;
+        }
+
+        public Node Find(int key)
+        {
+            if (count == 0)
+            {
+                return null;
+            }
+
+            Node current = Head;
+            while (current.Value != key)
+            {
+                current = current.Next;
+                if (current == null)
+                {
+                    return null;
+                }
+            }
+
+            return current;
+        }
+
+        private Node FindTail()
+        {
+            if (count == 0)
+            {
+                return null;
+            }
+
+            Node current = Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            return current;
+        }
+
+        public void PushBack(int item)
+        {
+            Node newNode = new Node(item);
+            if (count == 0)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node tail = FindTail();
+                tail.Next = newNode;
+            }
+
+            count++;
+        }
+
+        public void PushFront(int item)
+        {
+            Node newNode = new Node(item);
+
+            if (count != 0)
+            {
+                newNode.Next = Head;
+            }
+
+            Head = newNode;
+            count++;
+        }
+
+        public void AddAfter(Node node, int item)
+        {
+            Node newNode = new Node(item);
+
+            newNode.Next = node.Next;
+            node.Next = newNode;
+
+            count++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (count == 0)
+            {
+                throw new Exception("Список пуст");
+            }
+
+            Head = Head.Next;
+            count--;
+        }
+
+        public void RemoveLast()
+        {
+            if (count == 0)
+            {
+                throw new Exception("Список пуст");
+            }
+
+            if (count == 1)
+            {
+                Head = null;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = null;
+            }
+
+            count--;
+        }
+    }
+}
