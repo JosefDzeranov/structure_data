625a4672e9d779fb27b4f1b58cef32c5ea4d8de9
Josef Dzeranov <soslan29@gmail.com>
2020-06-16 14:55:01 +0300

    Implement QueueOnArray

--- StructureData/QueueOnArray/QueueOnArray.cs
+++ StructureData/QueueOnArray/QueueOnArray.cs
@@ -1,6 +1,63 @@
-﻿namespace QueueOnArray
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueOnArray
 {
     public class QueueOnArray
     {
+        private List<int> items;
+
+        public QueueOnArray()
+        {
+            items = new List<int>();
+        }
+
+        public string Print()
+        {
+            string result = "";
+            foreach (var item in items)
+            {
+                result += item + " ";
+            }
+
+            return result;
+        }
+
+        public bool Empty()
+        {
+            return items.Count == 0;
+        }
+
+        public void Push(int item)
+        {
+            items.Add(item);
+        }
+
+        public int Pop()
+        {
+            if (items.Count == 0)
+            {
+                throw new Exception("Очередь пустая.");
+            }
+
+            int lastItem = items[0];
+            items.RemoveAt(0);
+            return lastItem;
+        }
+
+        public int Peek()
+        {
+            if (items.Count == 0)
+            {
+                throw new Exception("Очередь пустая.");
+            }
+
+            return items[0];
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+        }
     }
 }
