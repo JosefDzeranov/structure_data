c2a1128ba35741355e80e40c6c1bd43f1341f351
Josef Dzeranov <soslan29@gmail.com>
2020-09-08 09:54:44 +0300

    implement QueueOnCircleArray

---
+++ StructureData/QueueOnCircleArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace QueueOnCircleArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            QueueOnCircleArray queue = new QueueOnCircleArray();
+            queue.Push(3); // Добавить 3
+            queue.Push(4); // Добавить 4
+            queue.Push(6); // Добавить 6
+            Console.WriteLine(queue.Print()); // выводит 3 4 6
+            
+            int deletedItem = queue.Pop(); // удалить первый элемент
+            Console.WriteLine(deletedItem); // выводит 3
+            Console.WriteLine(queue.Print()); // выводит 4 6
+            
+            deletedItem = queue.Pop(); // удалить первый элемент
+            Console.WriteLine(deletedItem); // выводит 4
+            Console.WriteLine(queue.Print()); // выводит 6
+            
+            deletedItem = queue.Pop(); // удалить первый элемент
+            Console.WriteLine(deletedItem); // выводит 6
+            Console.WriteLine(queue.Print()); // выводит пустую строку
+            
+            queue.Pop(); // Ошибка
+        }
+    }
+}
